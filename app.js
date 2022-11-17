94043218089d9e761a7dad73680939646d916e8e
iSushaant <kumar.su5hant@yahoo.com>
2022-11-17 13:42:09 +0530

    challenge 8 and 9 - console log the post request and styling the page - done

--- app.js
+++ app.js
@@ -32,11 +32,15 @@ app.get("/contact", (req, res)=>{
 });
 
 
+
 //Compose
 app.get("/compose", (req, res)=>{
   res.render('compose');
 });
 
+app.post('/compose', (req, res)=>{
+  console.log(req.body.blog);
+})
 
 
 
